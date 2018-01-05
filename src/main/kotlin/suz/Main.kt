bf9e8e89f6d406e36ad8b68791eb905c89e28b6f
khrom <devs@outlook.jp>
2018-01-05 19:07:57 +0900

    fix すごい整形した👍

--- src/main/kotlin/suz/Main.kt
+++ src/main/kotlin/suz/Main.kt
@@ -1,87 +1,12 @@
 package suz
 
-import org.nd4j.linalg.api.ndarray.INDArray
-import org.nd4j.linalg.factory.Nd4j
-import org.nd4j.linalg.ops.transforms.Transforms
 import java.io.File
 
 fun main(args: Array<String>) {
-    val HU = 196
-    val OU = 2 //鳥か烏か 01 10
-    val TORI = Nd4j.zeros(2, 1).put(0, 0, 1)
-    val KARASU = Nd4j.zeros(2, 1).put(1, 0, 1)
-    val LAMBDA = 0.0001
 
-    var images = LoadFeature.loadFeatureArray(File("./data/1/"))
-    val IU = images[0].size(0) * images[0].size(1)
-    var indexArray = Nd4j.zeros(1, 1)
-
-    var xArray = Nd4j.zeros(1, 1)
-    images.withIndex().forEach { (i, v) ->
-        xArray = if (i != 0) Nd4j.hstack(xArray, v.reshape(IU, 1)) else v.reshape(IU, 1)
-        indexArray = if (i != 0) Nd4j.hstack(indexArray, TORI) else TORI //Indexに鳥行列を追加
-    }
-    val images2 = LoadFeature.loadFeatureArray(File("./data/2/"))
-    images2.withIndex().forEach { (i, v) ->
-        xArray = Nd4j.hstack(xArray, v.reshape(IU, 1))
-        indexArray = Nd4j.hstack(indexArray, KARASU) //Indexに烏行列を追加
-    }
-    images.addAll(images2)
-
-    var wWeightNDArray = Nd4j.randn(HU, IU).mul(0.1)
-    var bBiasNDArray = Nd4j.randn(HU, 1).mul(0.1)
-    var uWeightNDArray = Nd4j.randn(OU, HU).mul(0.1)
-    var cBiasArray = Nd4j.randn(OU, 1).mul(0.1)
-
-    while (true) {//エポック回し
-        for ((index, input) in images.withIndex()) {
-            val y = sigmoidNuron(input.reshape(IU, 1), wWeightNDArray, bBiasNDArray)
-            val z = sigmoidNuron(y, uWeightNDArray, cBiasArray)
-            val deltaOut = z.sub(indexArray.getColumn(index)).mul(z.mul(z.rsub(1)))
-            val deltaHidden = deltaOut.transpose().mmul(uWeightNDArray).transpose().mul(y.mul(y.rsub(1)))
-
-            val du = deltaOut.mmul(y.transpose())
-            val dc = deltaOut.mul(-1)
-            val dw = deltaHidden.mmul(input.reshape(IU, 1).transpose())
-            val db = deltaHidden.mul(-1)
-
-            uWeightNDArray = uWeightNDArray.sub(du.mul(LAMBDA))
-            cBiasArray = cBiasArray.sub(dc.mul(LAMBDA))
-            wWeightNDArray = wWeightNDArray.sub(dw.mul(LAMBDA))
-            bBiasNDArray = bBiasNDArray.sub(db.mul(LAMBDA))
-
-        }
-//        for (input in images) {
-//            val ys = sigmoidNuron(input.reshape(IU, 1), wWeightNDArray, bBiasNDArray)
-//            val zs = sigmoidNuron(ys, uWeightNDArray, cBiasArray)
-//            println(zs)
-//        }
-        val ys = sigmoidNuron(xArray, wWeightNDArray, bBiasNDArray)
-        val zs = sigmoidNuron(ys, uWeightNDArray, cBiasArray)
-//        println(ys)
-//        println(zs)
-        val cost = costFunction(zs, indexArray).sumNumber().toDouble()
-//        for (img in images) {
-//            val ys1 = sigmoidNuron(img.reshape(IU, 1), wWeightNDArray, bBiasNDArray)
-//            val zs1 = sigmoidNuron(ys1, uWeightNDArray, cBiasArray)
-//            println("test$zs1")
-//        }
-
-        println(cost)
-
-    }
-}
-
-fun sigmoidNuron(target: INDArray, weightArray: INDArray, biasVector: INDArray): INDArray {
-    val p = weightArray.mmul(target).subColumnVector(biasVector).mul(-1)
-    val denominator = Transforms.exp(p).add(1)
-    return denominator.rdiv(1)
-}
-
-fun costFunction(target: INDArray, answer: INDArray): INDArray {
-    println(target)
-//    println(answer)
-    val c = target.sub(answer)
-//    println(c)
-    return Nd4j.sum(Transforms.pow(c, 2))
+    val toriDataArray = loadFeatureArray(File("./data/1/"))
+    val karasuDataArray = loadFeatureArray(File("./data/2/"))
+    var predict = Prediction(toriDataArray, karasuDataArray)
+    predict.train(epsiron = 1.0)
+    predict.save()
 }
---
+++ src/main/kotlin/suz/Prediction.kt
@@ -0,0 +1,107 @@
+package suz
+
+import org.nd4j.linalg.api.ndarray.INDArray
+import org.nd4j.linalg.dataset.DataSet
+import org.nd4j.linalg.factory.Nd4j
+import org.nd4j.linalg.ops.transforms.Transforms
+import java.io.File
+import kotlin.math.absoluteValue
+
+class Prediction(val toriDataArray: List<INDArray>, val karasuDataArray: List<INDArray>) {
+    val HU = 42 //隠れ層
+    val OU = 2 //出力：鳥か烏か 01 10
+    val TORI = Nd4j.zeros(2, 1).put(0, 0, 1)
+    val KARASU = Nd4j.zeros(2, 1).put(1, 0, 1)
+    val LAMBDA = 0.0001
+    val IU = Math.pow((ROWS_AND_COLUMNS * 2).toDouble(), 2.0).toInt()//画素サイズ 14*14
+
+    var wWeightNDArray = Nd4j.randn(HU, IU).mul(0.1)
+    var bBiasNDArray = Nd4j.randn(HU, 1).mul(0.1)
+    var uWeightNDArray = Nd4j.randn(OU, HU).mul(0.1)
+    var cBiasNDArray = Nd4j.randn(OU, 1).mul(0.1)
+
+
+    fun train(epsiron: Double = 0.01) {
+        var lastCostValue = 99999.0
+        var epoc = 1
+        var images = mutableListOf<INDArray>()
+
+        var indexArray = Nd4j.zeros(1, 1)
+
+        var xArray = Nd4j.zeros(1, 1)
+
+        toriDataArray.withIndex().forEach { (i, v) ->
+            xArray = if (i != 0) Nd4j.hstack(xArray, v.reshape(IU, 1)) else v.reshape(IU, 1)
+            indexArray = if (i != 0) Nd4j.hstack(indexArray, TORI) else TORI //Indexに鳥行列を追加
+        }
+        images.addAll(toriDataArray)
+
+        karasuDataArray.withIndex().forEach { (i, v) ->
+            xArray = Nd4j.hstack(xArray, v.reshape(IU, 1))
+            indexArray = Nd4j.hstack(indexArray, KARASU) //Indexに烏行列を追加
+        }
+        images.addAll(karasuDataArray)
+
+        while (true) {//エポック回し
+            for ((index, input) in images.withIndex()) {
+
+                val y = sigmoidNuron(input.reshape(IU, 1), wWeightNDArray, bBiasNDArray)
+                val z = sigmoidNuron(y, uWeightNDArray, cBiasNDArray)
+                val deltaOut = z.sub(indexArray.getColumn(index)).mul(z.mul(z.rsub(1)))
+                val deltaHidden = deltaOut.transpose().mmul(uWeightNDArray).transpose().mul(y.mul(y.rsub(1)))
+
+                val du = deltaOut.mmul(y.transpose())
+                val dc = deltaOut.mul(-1)
+                val dw = deltaHidden.mmul(input.reshape(IU, 1).transpose())
+                val db = deltaHidden.mul(-1)
+
+                uWeightNDArray = uWeightNDArray.sub(du.mul(LAMBDA))
+                cBiasNDArray = cBiasNDArray.sub(dc.mul(LAMBDA))
+                wWeightNDArray = wWeightNDArray.sub(dw.mul(LAMBDA))
+                bBiasNDArray = bBiasNDArray.sub(db.mul(LAMBDA))
+
+            }
+            val ys = sigmoidNuron(xArray, wWeightNDArray, bBiasNDArray)
+            val zs = sigmoidNuron(ys, uWeightNDArray, cBiasNDArray)
+            val cost = costFunction(zs, indexArray).sumNumber().toDouble()
+            println("EPOC:${epoc}\tcost:${cost}")
+            epoc++
+            if ((lastCostValue - cost).absoluteValue <= epsiron) return //閾値以下になったら終了
+            lastCostValue = cost
+        }
+    }
+
+    fun save() {
+        DataSet(wWeightNDArray, Nd4j.zeros(1, 1)).save(File("./wWeight"))
+        DataSet(bBiasNDArray, Nd4j.zeros(1, 1)).save(File("./bBias"))
+        DataSet(uWeightNDArray, Nd4j.zeros(1, 1)).save((File("./uWeight")))
+        DataSet(cBiasNDArray, Nd4j.zeros(1, 1)).save((File("./cBias")))
+    }
+
+    fun load(wWeightFile: File = File("./wWeight"),
+             bBiasFile: File = File("./bBias"),
+             uWeightFile: File = File("./uWeight"),
+             cBiasFile: File = File("./cBias")) {
+        var data = DataSet()
+        data.load(wWeightFile)
+        wWeightNDArray = data.featureMatrix
+        data.load(bBiasFile)
+        bBiasNDArray = data.featureMatrix
+        data.load(uWeightFile)
+        uWeightNDArray = data.featureMatrix
+        data.load(cBiasFile)
+        cBiasNDArray = data.featureMatrix
+
+    }
+
+    private fun sigmoidNuron(target: INDArray, weightArray: INDArray, biasVector: INDArray): INDArray {
+        val p = weightArray.mmul(target).subColumnVector(biasVector).mul(-1)
+        val denominator = Transforms.exp(p).add(1)
+        return denominator.rdiv(1)
+    }
+
+    private fun costFunction(target: INDArray, answer: INDArray): INDArray {
+        val c = target.sub(answer)
+        return Nd4j.sum(Transforms.pow(c, 2))
+    }
+}
