552d16a47b21e5de4f9aa7293cb9d4c205d814a8
Khromium <devs@outlook.jp>
2018-01-29 01:24:27 +0900

    fix 使わないコードの削除とリファクタリング

--- src/main/kotlin/suz/LoadFeature.kt
+++ src/main/kotlin/suz/LoadFeature.kt
@@ -11,23 +11,13 @@ val DATA_SIZE = Math.pow((ROWS_AND_COLUMNS * 2).toDouble(), 2.0).toInt()//画素
 
 /**
  * 7x7の畳み込み画像を読み込む
- * 7x7の画像は特徴量が4方向用意されているので14x14の行列に拡張して処理を行う
  */
 fun load(file: File): INDArray {
     var textString = file.readText(Charset.defaultCharset())
-    val data = textString.split("\r\n|\n".toRegex())
-    var featuresArray = mutableListOf<INDArray>()
-    for (i: Int in 0..(FEATURE_NUM - 1)) {
-        var features = Nd4j.zeros(ROWS_AND_COLUMNS, ROWS_AND_COLUMNS)
-        for (j: Int in 0..(ROWS_AND_COLUMNS - 1)) {
-            for (k: Int in 0..(ROWS_AND_COLUMNS - 1)) {
-                features.put(j, k, Integer.parseInt(data[i * ROWS_AND_COLUMNS * ROWS_AND_COLUMNS + j * ROWS_AND_COLUMNS + k]))
-            }
-        }
-        featuresArray.add(features)
-    }
-    //14x14の行列を返す
-    return Nd4j.vstack(Nd4j.hstack(featuresArray[0], featuresArray[1]), Nd4j.hstack(featuresArray[2], featuresArray[3]))
+    var data = textString.split("\r\n|\n".toRegex())
+    var dataArray = DoubleArray(DATA_SIZE)
+    data.withIndex().forEach { if (it.index < DATA_SIZE) dataArray[it.index] = it.value.toDouble() }
+    return Nd4j.create(dataArray)
 }
 
 /**
@@ -38,23 +28,6 @@ fun loadFeatureArray(rootFolder: File): List<INDArray> {
     if (!rootFolder.isDirectory) return featureArray
     rootFolder.listFiles().filter { it.name.contains(".\\.txt".toRegex()) }.sorted().mapTo(featureArray) { load(it) }
     return featureArray
-}
-
-fun loadLayer(file: File): MutableList<INDArray> {
-    var textString = file.readText(Charset.defaultCharset())
-    val data = textString.split("\n")
-    var featuresArray = mutableListOf<INDArray>()
-    for (i: Int in 0..(FEATURE_NUM - 1)) {
-        var features = Nd4j.zeros(ROWS_AND_COLUMNS, ROWS_AND_COLUMNS)
-        for (j: Int in 0..(ROWS_AND_COLUMNS - 1)) {
-            for (k: Int in 0..(ROWS_AND_COLUMNS - 1)) {
-                features.put(j, k, Integer.parseInt(data[i * ROWS_AND_COLUMNS * ROWS_AND_COLUMNS + j * ROWS_AND_COLUMNS + k]))
-            }
-        }
-        featuresArray.add(features)
-    }
-    //14x14の行列を返す
-    return featuresArray
 }
 
 
